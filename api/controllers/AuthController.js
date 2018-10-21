6792e20c6cbe842c2aadd54ca501b452a0f8b0ae
o5faruk <faruk.alibasic@gmail.com>
2018-10-21 12:20:31 +0200

    login

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User'),
-    jwt = require('jsonwebtoken')
+    bcrypt = require('bcrypt'),
+    tokenCreator = require('../services/token/create')
 
 module.exports = {
     register: async (req, res) => {
@@ -8,9 +9,31 @@ module.exports = {
         );
         try {
             await newUser.save()
-            return res.json(newUser);
+            return res.json(newUser)
         } catch (error) {
-            return res.json(error);
+            return res.status(400).json(error)
+        }
+    },
+    login: async (req, res) => {
+        const email = req.body.email
+        const password = req.body.password
+
+        if (!email || !password) return res.status(401).send({ "message": "Bad Credentials" })
+
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(401).send({ "message": "Bad Credentials" })
+        }
+        try {
+            let password_ok = await bcrypt.compare(password, user.password)
+            if (password_ok) {
+                const token = await tokenCreator({ _id: user._id });
+                return res.json({ token })
+            } else {
+                return res.status(401).send({ "message": "Bad Credentials" })
+            }
+        } catch (error) {
+            return res.status(500).send({ message: "Server Error" })
         }
     }
 }
