61d1d1960010decde4d46cc64fc7e17a6d09ca01
o5faruk <faruk.alibasic@gmail.com>
2018-10-21 11:13:50 +0200

    password protection & hashing

--- api/models/User.js
+++ api/models/User.js
@@ -1,18 +1,41 @@
-let mongoose = require('mongoose'),
-    Schema = mongoose.Schema()
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bcrypt = require('bcrypt'),
+    uniqueValidator = require('mongoose-unique-validator');
 
-let User = new Schema({
+
+const User = new Schema({
     email: {
         type: String,
         lowercase: true,
         unique: true,
+        trim: true,
         required: 'Email is required!'
     },
     password: {
-        type: String
+        type: String,
+        trim: true,
+        required: 'Password is required!'
     },
-
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
 })
+
+User.plugin(uniqueValidator)
+
+User.pre('save', async function (next) {
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+User.methods.toJSON = function () {
+    var obj = this.toObject()
+    delete obj.password
+    delete obj.__v
+    return obj
+}
 
 module.exports = mongoose.model('User', User)
 
