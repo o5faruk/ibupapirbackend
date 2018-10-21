a47184c895b05bf5299b61232053548ba3e7ab98
o5faruk <faruk.alibasic@gmail.com>
2018-10-21 11:13:27 +0200

    add new user

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,5 +1,16 @@
+const User = require('../models/User'),
+    jwt = require('jsonwebtoken')
+
 module.exports = {
-    // register: (req, res) => {
-    //     return res.json({ re: "re" })
-    // }
+    register: async (req, res) => {
+        const newUser = new User(
+            req.body
+        );
+        try {
+            await newUser.save()
+            return res.json(newUser);
+        } catch (error) {
+            return res.json(error);
+        }
+    }
 }
