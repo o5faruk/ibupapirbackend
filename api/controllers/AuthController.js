36d6bd1af25ef97888d11ced623e8fad099ba52a
o5faruk <faruk.alibasic@gmail.com>
2018-10-21 12:25:52 +0200

    formatting

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -4,9 +4,7 @@ const User = require('../models/User'),
 
 module.exports = {
     register: async (req, res) => {
-        const newUser = new User(
-            req.body
-        );
+        const newUser = new User(req.body)
         try {
             await newUser.save()
             return res.json(newUser)
@@ -27,7 +25,7 @@ module.exports = {
         try {
             let password_ok = await bcrypt.compare(password, user.password)
             if (password_ok) {
-                const token = await tokenCreator({ _id: user._id });
+                const token = await tokenCreator({ _id: user._id })
                 return res.json({ token })
             } else {
                 return res.status(401).send({ "message": "Bad Credentials" })
